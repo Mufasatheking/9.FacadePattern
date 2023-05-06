ee4354b439591b895d4098bc8e4d8148699bdabb
Mustafa Aleem <maleem@intralinks.com>
2023-05-06 23:00:37 +0100

    FacadePattern

---
+++ FacadePattern/RobotColor.cs
@@ -0,0 +1,14 @@
+namespace FacadePattern
+{
+    public class RobotColor
+    {
+        public void SetDefaultColor()
+        {
+            Console.WriteLine("This is steel color robot.");
+        }
+        public void SetGreenColor()
+        {
+            Console.WriteLine("This is a green color robot.");
+        }
+    }
+}
